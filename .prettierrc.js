47cf07421c6e11a875f1f39a81d11a31c27e7012
valdisneinilo <valdisneinilo@gmail.com>
2022-11-25 10:49:28 -0300

    estrutura base do projeto

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: true, // ponto e vírgula
+  trailingComma: "all", // manter virgula no final de objetos e arrays
+  singleQuote: false, // aspas simples
+  printWidth: 80, // tamanho da linha
+  tabWidth: 2, // tamanho da tabulação
+};
